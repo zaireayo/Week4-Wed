bd776fc0c200c68c6ae8ff7953c77b965e3a8c28
zaireayo <zaire.garza@hgs.hiddengeniusproject.org>
2018-10-09 15:46:51 -0700

    first commit

---
+++ week4-wed.sh
@@ -0,0 +1,14 @@
+cd ~/Desktop
+mkdir Week4-Wed
+cd Week4-Wed
+mkdir css
+mkdir img
+touch index.html
+cd css
+touch style1.css
+touch style2.css
+cd ../
+cd img
+curl -o img1.jpg "https://images.champssports.com/is/image/EBFL2/100074M_a1?hei=1500&wid=1500"
+curl -o img2.jpg "https://i.pinimg.com/236x/a0/3d/08/a03d08e666c637eb1359bd142e032f52.jpg"
+curl -o img3.jpg "https://bandlabimages.azureedge.net/v1.0/users/71e8826a-0c02-e811-80c2-00155d60b1c1/636577097383072611/1024x1024"
